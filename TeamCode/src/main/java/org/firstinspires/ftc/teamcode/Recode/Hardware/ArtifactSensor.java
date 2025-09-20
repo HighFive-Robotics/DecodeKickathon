b99565158248a54d101f1221f55405b479834a4e
MariaTrandafir39 <maria@highfive.ro>
2025-09-20 16:14:35 +0300

    artifact sensor start

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Recode/Hardware/ArtifactSensor.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.Recode.Hardware;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@Config
+public class ArtifactSensor {
+
+    public ArtifactSensor(HardwareMap hardwareMap)
+    {
+        
+    }
+
+}
