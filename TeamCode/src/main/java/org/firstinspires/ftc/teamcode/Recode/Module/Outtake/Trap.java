7a77cc63cdcc2b00b5a3f5fb46881b3a5108fb4b
Teeeeoo <teo10.pana@yahoo.com>
2025-09-27 14:43:20 +0300

    Finished Trap

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Recode/Module/Outtake/Trap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Recode/Module/Outtake/Trap.java
@@ -1,32 +1,82 @@
 package org.firstinspires.ftc.teamcode.Recode.Module.Outtake;
 
+import static org.firstinspires.ftc.teamcode.Constants.DeviceNames.trapSensorName;
+import static org.firstinspires.ftc.teamcode.Constants.DeviceNames.trapServoName;
+import static org.firstinspires.ftc.teamcode.Constants.currentColor;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.Constants;
+import org.firstinspires.ftc.teamcode.Core.Hardware.ArtifactSensor;
 import org.firstinspires.ftc.teamcode.Core.Hardware.HighModule;
 import org.firstinspires.ftc.teamcode.Core.Hardware.HighServo;
 
+@Config
 public class Trap implements HighModule {
     public HighServo trapServo;
-    @Override
-    public void setTarget(double target) {
+    public ArtifactSensor sensor;
+    public static double openPose = 0.55, closedPose = 0.8;
+    public static Constants.Color wantedColor = Constants.Color.Blue;
+    public States state= States.None;
+    public enum States
+    {
+        Closed,
+        Open,
+        None
+    }
+    private double target;
 
+    public Trap(HardwareMap hardwareMap, double initPos, boolean isAuto)
+    {
+        trapServo=new HighServo(hardwareMap.get(Servo.class, trapServoName), HighServo.RunMode.Standard, initPos, isAuto);
+        sensor=new ArtifactSensor(hardwareMap, trapSensorName);
+        target=initPos;
     }
 
-    @Override
-    public void setTarget(double target, double time) {
-
+    public void setState(States state, double time)
+    {
+        this.state = state;
+        switch (state)
+        {
+            case Closed:
+                if(time == 0)setTarget(closedPose);
+                else setTarget(closedPose, time);
+                break;
+            case Open:
+                if(time == 0)setTarget(openPose);
+                else setTarget(openPose, time);
+                break;
+        }
     }
-
     @Override
-    public boolean atTarget() {
-        return false;
+    public void setTarget(double target)
+    {
+        this.target = target;
+        trapServo.setPosition(target);
     }
-
     @Override
-    public double getTarget() {
-        return 0;
+    public void setTarget(double target, double time)
+    {
+        this.target = target;
+        trapServo.setPosition(target, time);
     }
-
     @Override
-    public void update() {
-
+    public boolean atTarget() {return trapServo.atTarget();}
+    @Override
+    public double getTarget() {return target;}
+    public void setWantedColor(Constants.Color color){
+        wantedColor = color;
+    }
+    @Override
+    public void update()
+    {
+        if(wantedColor == currentColor && state != States.Open)
+        {
+            setState(States.Open, trapServo.time);
+        }
+        trapServo.update();
+        sensor.update();
     }
 }
